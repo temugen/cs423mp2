1e63d7643ecc12f0441117c439f49405dfb4b42d
Brad Misik <temugen@bradmisik.com>
2011-09-28 13:47:30 -0500

    Removed erroneous printks

--- mp2.c
+++ mp2.c
@@ -90,7 +90,6 @@ void up_handler(unsigned long ptr)
 {
     struct task *t = (struct task *)ptr;
     BUG_ON(t == NULL);
-    printk("%p\n", t);
     set_timer(&t->wakeup_timer, t->period);
     t->state = READY;
 
@@ -105,7 +104,6 @@ int register_task(unsigned long pid, unsigned long period, unsigned long computa
     if (_lookup_task(pid) != NULL) return -1;
 
     newtask = kmalloc(sizeof(struct task),GFP_KERNEL);
-    printk("newtask: %p\n", newtask);
     newtask->pid = pid;
     timer_init(&newtask->wakeup_timer, up_handler);
     newtask->wakeup_timer.data = (unsigned long)newtask;
@@ -171,7 +169,6 @@ int proc_registration_write(struct file *file, const char *buffer, unsigned long
 
             if(t->state == SLEEPING) //THIS IS OUR FIRST YIELD
             {
-                printk("set timer\n");
                 set_timer(&t->wakeup_timer, t->period);
                 t->state = READY;
             }
@@ -230,11 +227,6 @@ int context_switch(void *data)
         printk(KERN_ALERT "CONTEXT SWITCH\n");
         next_task = _get_next_task();
         mutex_unlock(&mutex);
-
-        if(next_task)
-            printk("next: %u, state: %u\n", next_task->pid, next_task->state);
-        if(currtask)
-            printk("curr: %u, state: %u\n", currtask->pid, currtask->state);
 
         if(next_task == currtask)
             goto same_task;
