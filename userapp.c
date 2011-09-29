97093c59452fe68f3072356ac2f540f9b3bf14df
Brad Misik <temugen@bradmisik.com>
2011-09-29 10:00:43 -0500

    Updated userapp to check for admission and print time differences

--- userapp.c
+++ userapp.c
@@ -1,46 +1,75 @@
 #include <stdio.h>
 #include <sys/syscall.h>
+#include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
 #include "userapp.h"
 
+#define PROC_FILENAME "//proc/mp2/status"
+
 unsigned long long factorial(int number)
 {
-  unsigned long long retval=1;
-  unsigned long long i;
+    unsigned long long retval=1;
+    unsigned long long i;
 
-  for (i=1; i <= number; i++)
-  {
-    retval=retval *i;
-  }
+    for (i=1; i <= number; i++)
+    {
+        retval=retval *i;
+    }
 
-  return retval;
+    return retval;
 }
 
 int main(int argc, char* argv[])
 {
-  char cmd[120];
-  pid_t mypid;
-  int j,k;
+    char cmd[120];
+    pid_t mypid;
+    int j,k;
+    char line[120];
+    FILE *file;
+    unsigned long int pid, period, computation;
+    struct timeval last_tv, current_tv;
 
-  mypid= syscall(__NR_gettid);
-  sprintf(cmd, "echo 'R, %u, 2000, 20'>//proc/mp2/status", mypid);
-  system(cmd);
+    mypid= syscall(__NR_gettid);
+    sprintf(cmd, "echo 'R, %u, 2000, 20'>" PROC_FILENAME, mypid);
+    system(cmd);
 
-  sprintf(cmd, "echo 'Y, %u'>//proc/mp2/status", mypid);
-  system(cmd);
+    file = fopen(PROC_FILENAME, "r");
+    while(fgets(line, sizeof(line), file) != NULL)
+    {
+        sscanf(line, "%lu: %lu %lu\n", &pid, &period, &computation);
+        if(pid == mypid)
+            break;
+    }
+    fclose(file);
 
-  struct timeval tv;
+    if(pid != mypid)
+    {
+        printf("Could not schedule our task\n");
+        return -1;
+    }
 
-  //for (k=0;k<10000; k++)
+    gettimeofday(&last_tv, NULL);
+    sprintf(cmd, "echo 'Y, %u'>" PROC_FILENAME, mypid);
+    system(cmd);
+
     for(j=0; j<10; j++)
     {
-        gettimeofday(&tv, NULL);
-        printf("%llu %llu\n", tv.tv_sec, tv.tv_usec);
-        printf("Factorial %u: %llu\n",j, factorial(j));
-        sprintf(cmd, "echo 'Y, %u'>//proc/mp2/status", mypid);
+        gettimeofday(&current_tv, NULL);
+
+        printf("%llu sec %llu us | fact(%u): %llu\n",
+                current_tv.tv_sec - last_tv.tv_sec,
+                current_tv.tv_usec - last_tv.tv_usec,
+                j, factorial(j));
+
+        last_tv = current_tv;
+
+        sprintf(cmd, "echo 'Y, %u'>" PROC_FILENAME, mypid);
         system(cmd);
     }
-    sprintf(cmd, "echo 'D, %u'>//proc/mp2/status", mypid);
+
+    sprintf(cmd, "echo 'D, %u'>" PROC_FILENAME, mypid);
     system(cmd);
+
+    return 0;
 }
